1c8cd49a37448253d558325afc8868d5e5102fdf
Judy2k <judy@judy.co.uk>
2020-10-12 10:12:19 +0100

    A bunch of changes and improvements to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod data;
 
 use crate::data::Recipe;
+
+use chrono::Utc;
 use futures::stream::StreamExt;
 use mongodb::bson::{doc, from_document, Bson};
 use mongodb::{options::ClientOptions, options::FindOptions, options::ResolverConfig, Client};
@@ -24,56 +26,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let recipes = database.collection("recipes");
 
     let options = FindOptions::builder().sort(doc! { "name": 1 }).build();
-    let mut cursor = recipes.find(None, options).await?;
+    let mut cursor = recipes.find(None, options.clone()).await?;
     heading("All Cocktails");
     while let Some(result) = cursor.next().await {
-        match result {
-            Ok(recipe) => {
-                if let Some(name) = recipe.get("name").and_then(Bson::as_str) {
-                    println!("Cocktail: {}", name);
-                } else {
-                    println!("no name found");
-                }
-            }
-            Err(e) => return Err(e.into()),
-        }
+        let recipe = result?;
+        println!("Cocktail: {}", recipe.get_str("name")?);
+    }
+
+    /*
+    recipes.insert_one(doc! {
+        "name": "Dodgy Cocktail",
+        "ingredients": [{
+            "name": "Water",
+            "quantity": { "unit": "ml", "amount": 30 }
+        }],
+        "instructions": [
+            "Pour yourself some water from the tap."
+        ]
+    }, None).await?;
+    */
+
+    // Search by name:
+    heading("Negroni Sbagliato");
+    let query = doc! { "name": "Negroni Sbagliato" };
+    let mut cursor = recipes.find(query, options.clone()).await?;
+    while let Some(result) = cursor.next().await {
+        let recipe = result?;
+        println!("Cocktail: {}", recipe.get_str("name")?);
     }
 
     // Vodka Cocktails:
     heading("Vodka Cocktails");
-    let options = FindOptions::builder().sort(doc! { "name": 1 }).build();
-    let mut cursor = recipes
-        .find(
-            doc! { "ingredients": { "$elemMatch": {"name": "Vodka"} } },
-            options,
-        )
-        .await?;
+    let query = doc! { "ingredients": { "$elemMatch": {"name": "Vodka"} } };
+    let mut cursor = recipes.find(query, options.clone()).await?;
     while let Some(result) = cursor.next().await {
-        match result {
-            Ok(recipe) => {
-                if let Some(name) = recipe.get("name").and_then(Bson::as_str) {
-                    println!("Cocktail: {}", name);
-                } else {
-                    println!("no name found");
-                }
-            }
-            Err(e) => return Err(e.into()),
-        }
+        let recipe = result?;
+        println!("Cocktail: {}", recipe.get_str("name")?);
     }
 
     let mut cursor = recipes.aggregate(vec![], None).await?;
     heading("Empty Aggregation Pipeline");
     while let Some(result) = cursor.next().await {
-        match result {
-            Ok(recipe) => {
-                if let Some(name) = recipe.get("name").and_then(Bson::as_str) {
-                    println!("Cocktail: {}", name);
-                } else {
-                    println!("no name found");
-                }
-            }
-            Err(e) => return Err(e.into()),
-        }
+        let recipe = result?;
+        println!("Cocktail: {}", recipe.get_str("name")?);
     }
 
     /*
@@ -122,16 +117,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     heading("Sorting");
     while let Some(result) = cursor.next().await {
-        match result {
-            Ok(recipe) => {
-                if let Some(name) = recipe.get("name").and_then(Bson::as_str) {
-                    println!("Cocktail: {}", name);
-                } else {
-                    println!("no name found");
-                }
-            }
-            Err(e) => return Err(e.into()),
-        }
+        let recipe = result?;
+        println!("Cocktail: {}", recipe.get_str("name")?);
     }
 
     // Filtering:
@@ -151,29 +138,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cursor = recipes.aggregate(query.clone(), None).await?;
     heading("Filtering");
     while let Some(result) = cursor.next().await {
-        match result {
-            Ok(recipe) => {
-                if let Some(name) = recipe.get("name").and_then(Bson::as_str) {
-                    println!("Cocktail: {}", name);
-                } else {
-                    println!("no name found");
-                }
-            }
-            Err(e) => return Err(e.into()),
-        }
+        let recipe = result?;
+        println!("Cocktail: {}", recipe.get_str("name")?);
     }
 
     // Deserialization:
     let mut cursor = recipes.aggregate(query.clone(), None).await?;
     heading("Deserialization");
     while let Some(result) = cursor.next().await {
-        match result {
-            Ok(recipe) => {
-                let recipe: Recipe = from_document(recipe).unwrap();
-                println!("{}", recipe);
-            }
-            Err(e) => return Err(e.into()),
-        }
+        let recipe: Recipe = from_document(result?).unwrap();
+        println!("{}", recipe);
     }
 
     // Join:
@@ -209,13 +183,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    // Aggregate:
     let query = vec![
         doc! {
-            "$match": {
-                "name": { "$lte": "Addison" }
-            }
-        },
+        "$match": {
+            "ingredients": {
+                "$elemMatch": {
+                    "name": "Vodka"}}}},
+        doc! {
+        "$lookup": {
+            "from": "reviews",
+            "localField": "_id",
+            "foreignField": "recipe_id",
+            "as": "reviews"}},
+    ];
+
+    // Aggregate:
+    let query = vec![
         doc! {
             "$sort": {
                 "name": 1
@@ -236,18 +219,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 },
             },
         },
+        doc! {
+            "$sort": {
+                "rating": -1
+            }
+        },
+        doc! {
+            "$limit": 10
+        },
     ];
     let mut cursor = recipes.aggregate(query.clone(), None).await?;
-    heading("Lookup");
+    heading("10 Highest Reviewed");
     while let Some(result) = cursor.next().await {
         match result {
             Ok(recipe) => {
-                let recipe: Recipe = from_document(recipe).unwrap();
+                let recipe: Recipe = from_document(recipe)?;
                 println!("{}", recipe);
             }
             Err(e) => return Err(e.into()),
         }
     }
+
+    recipes.update_one(
+        doc! {"name": "Negroni Sbagliato"},
+        doc! {
+            "$push": {
+                "reviews": {
+                    "rating": 4,
+                    "when": Utc::now(),
+                }
+            },
+        },
+        None,
+    )
+    .await?;
 
     Ok(())
 }
